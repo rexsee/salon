207b0c71ed926ad875e01903ca4378fd721ee5b8
Rex <rex@whitebean.my>
2021-05-02 17:18:29 +0800

    - upgrade webpack libraries - add front plugins - add adminlte js and css

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-let mix = require('laravel-mix');
+const mix = require('laravel-mix');
 
 /*
  |--------------------------------------------------------------------------
@@ -14,10 +14,21 @@ let mix = require('laravel-mix');
 mix.combine([
     'resources/assets/js/jquery-3.2.1.min.js',
     'resources/assets/js/popper.min.js',
-    // 'resources/assets/js/jquery-ui.min.js',
     'resources/assets/js/bootstrap.min.js',
     'resources/assets/js/datatables.min.js'
     ],'public/js/app.js')
+    .combine([
+        'public/plugins/jquery/jquery.min.js',
+        'public/plugins/bootstrap/js/bootstrap.bundle.min.js',
+        'public/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js',
+        'public/plugins/datatables/jquery.dataTables.js',
+        'public/plugins/datatables-bs4/js/dataTables.bootstrap4.js',
+        'public/plugins/fontawesome/js/fontawesome-all.js',
+        'public/plugins/moment/moment.min.js',
+        'public/plugins/select2/js/select2.full.min.js',
+        'public/js/adminlte.min.js',
+        'public/js/noty.js',
+    ], 'public/js/adminlte.min.js')
     .combine([
         'resources/assets/js/jquery.min.js',
         'resources/assets/js/jquery-confirm.min.js',
@@ -27,6 +38,7 @@ mix.combine([
         'resources/assets/js/main.js'
     ],'public/js/front.js')
     .sass('resources/assets/sass/app.scss', 'public/css')
+    .sass('resources/assets/sass/adminlte.scss', 'public/css/admin.css')
     .sass('resources/assets/sass/front.scss', 'public/css')
     .copy('resources/assets/fonts','public/fonts')
     .copy('resources/assets/js/modernizr.min.js','public/js')
